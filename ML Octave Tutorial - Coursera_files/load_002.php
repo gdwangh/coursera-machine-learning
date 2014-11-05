39133cd35f0b6b9042af133eb1566a247da0e9c3
gdwangh <gdwangh@189.cn>
2014-11-06 07:06:00 +0800

    delete unused file. Please enter the commit message for your changes. Lines starting

--- ML Octave Tutorial - Coursera_files/load_002.php
+++
@@ -1,2 +0,0 @@
-mw.loader.implement("jquery.client", ["https://share.coursera.org/wiki/resources/jquery/jquery.client.js"], {}, {});
-mw.loader.implement("mediawiki.page.startup", ["https://share.coursera.org/wiki/resources/mediawiki.page/mediawiki.page.startup.js"], {}, {});
