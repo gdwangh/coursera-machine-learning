11f41132969cbc5b3296564fd73486146d384dcb
gdwangh <gdwangh@189.cn>
2014-11-06 01:34:21 +0800

    program ex1, part 1~6

---
+++ ML Octave Tutorial - Coursera_files/load_002.php
@@ -0,0 +1,2 @@
+mw.loader.implement("jquery.client", ["https://share.coursera.org/wiki/resources/jquery/jquery.client.js"], {}, {});
+mw.loader.implement("mediawiki.page.startup", ["https://share.coursera.org/wiki/resources/mediawiki.page/mediawiki.page.startup.js"], {}, {});
